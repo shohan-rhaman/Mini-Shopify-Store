d5a7e104f91764e1fa7c23e57cbdecf2bcf1edcd
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-08-04 15:21:38 +0600

    Project Setup

---
+++ data.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1,
+        name: "Nike Air Max",
+        category: "Shoes",
+        price: 120,
+        stock: 10,
+        image: "https://picsum.photos/300?1"
+    },
+    {
+        id: 2,
+        name: "Adidas Hoodie",
+        category: "Clothing",
+        price: 80,
+        stock: 5,
+        image: "https://picsum.photos/300?2"
+    },
+    {
+        id: 3,
+        name: "Apple Watch",
+        category: "Electronics",
+        price: 350,
+        stock: 8,
+        image: "https://picsum.photos/300?3"
+    },
+    {
+        id: 4,
+        name: "Puma Shoes",
+        category: "Shoes",
+        price: 95,
+        stock: 6,
+        image: "https://picsum.photos/300?4"
+    },
+    {
+        id: 5,
+        name: "Backpack",
+        category: "Accessories",
+        price: 45,
+        stock: 15,
+        image: "https://picsum.photos/300?5"
+    },
+    {
+        id: 6,
+        name: "Bluetooth Speaker",
+        category: "Electronics",
+        price: 150,
+        stock: 7,
+        image: "https://picsum.photos/300?6"
+    }
+];
